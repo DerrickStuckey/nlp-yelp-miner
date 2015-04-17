72f0ae7427c51536d7916d8f8c50f92d69cf3b0f
DerrickStuckey <derrickstuckey@gmail.com>
2015-04-16 21:10:28 -0400

    added visualization of dendrogram branches

--- R_Analysis/Uncle_Julios_hclust.R
+++ R_Analysis/Uncle_Julios_hclust.R
@@ -28,5 +28,30 @@ dtm_transpose <- t(dtm_matrix)
 scaled_dtm_t <- scale(dtm_transpose)
 d_t <- dist(scaled_dtm_t,method="canberra")
 term_hclust_fit <- hclust(d_t,method=method_in) #clusters terms
+
+# plot the dendrogram
 plot(term_hclust_fit,main="Term Hierarchical Clustering",xlab="Terms")
+
+# plot dendrogram with some cuts
+#op = par(mfrow = c(2, 1)) #???
+hcd = as.dendrogram(term_hclust_fit)
+plot(cut(hcd, h = 2000)$upper, main = "Upper tree of cut at h=2000")
+plot(cut(hcd, h = 2000)$lower[[1]], main = "First branch of lower tree with cut at h=2000")
+plot(cut(hcd, h = 2000)$lower[[2]], main = "Second branch of lower tree with cut at h=2000")
+plot(cut(hcd, h = 2000)$lower[[3]], main = "Third branch of lower tree with cut at h=2000")
+plot(cut(hcd, h = 2000)$lower[[4]], main = "Fourth branch of lower tree with cut at h=2000")
+
+fourth_cut <- cut(hcd, h = 2000)$lower[[4]]
+plot(cut(fourth_cut, h=800)$lower[[1]], main= "Branch 1 of branch 4")
+plot(cut(fourth_cut, h=800)$lower[[2]], main= "Branch 2 of branch 4")
+plot(cut(fourth_cut, h=800)$lower[[3]], main= "Branch 3 of branch 4")
+
+first_cut <- cut(hcd, h = 2000)$lower[[1]]
+plot(cut(first_cut, h=200)$lower[[1]], main= "Branch 1 of branch 4")
+plot(cut(first_cut, h=200)$lower[[2]], main= "Branch 2 of branch 4")
+plot(cut(first_cut, h=200)$lower[[3]], main= "Branch 3 of branch 4")
+plot(cut(first_cut, h=200)$lower[[4]], main= "Branch 4 of branch 4")
+plot(cut(first_cut, h=200)$lower[[5]], main= "Branch 5 of branch 4")
+plot(cut(first_cut, h=200)$lower[[6]], main= "Branch 6 of branch 4")
+
 
